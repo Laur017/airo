b463089f5c6907697316302ac897c0b533311cbf
ibordea <ibordea@griddynamics.com>
2025-03-12 11:05:14 +0200

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css';
 import Header from './components/Header/Header';
-import Settings from "./components/Settings/Settings";
+import Settings from './components/Settings/Settings';
 
 function App() {
 	return (
 		<>
 			<Header />
-      <Settings />
+			<Settings />
 		</>
 	);
 }
