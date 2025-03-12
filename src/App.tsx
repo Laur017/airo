5f3f93124b6314d2eb1e9b85da9fe114967efd63
ibordea <ibordea@griddynamics.com>
2025-03-12 13:51:30 +0200

    map

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css';
 import Header from './components/Header/Header';
-import Settings from './components/Settings/Settings';
+import Map from './components/Map/Map';
+// import Settings from './components/Settings/Settings';
 
 function App() {
 	return (
 		<div className='app'>
 			<Header />
 			<div className='main'>
-				<Settings />
+				{/* <Settings /> */}
+				<Map />
 			</div>
 		</div>
 	);
---
+++ src/components/Spinner/Spinner.tsx
@@ -0,0 +1,8 @@
+export default function Spinner() {
+	return (
+		<div className='spinner-container'>
+			<div className='spinner'></div>
+			<span className='loading-text'>Loading...</span>
+		</div>
+	);
+}
