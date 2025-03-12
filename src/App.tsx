9f4410c491b9948e2c35eed7781e89ac1c13e44d
Popa Laurentiu <popa.laur.01@gmail.com>
2025-03-12 11:02:42 +0200

    created the settings component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import './App.css';
-import Header from './components/Header/Header';
+import "./App.css";
+import Header from "./components/Header/Header";
 import Settings from "./components/Settings/Settings";
 
 function App() {
-	return (
-		<>
-			<Header />
-      <Settings />
-		</>
-	);
+  return (
+    <div className="app">
+      <Header />
+      <div className="main">
+        <Settings />
+      </div>
+    </div>
+  );
 }
 export default App;
