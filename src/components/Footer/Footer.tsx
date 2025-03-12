3d2f8633c27b3064bbb2dfd72bdd66f122436a0d
Popa Laurentiu <popa.laur.01@gmail.com>
2025-03-12 15:23:06 +0200

    implemented the footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import gridLogo from "../../assets/grid-logo.png";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <img src={gridLogo} alt="Grid Dynamics " />
+      <p>2025 @ All rights reserved. Developed by ibordea & lpopa</p>
+    </div>
+  );
+}
