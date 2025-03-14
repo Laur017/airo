fc51afe230e71c3777c0af147a32f7aa9ba7a889
ibordea <ibordea@griddynamics.com>
2025-03-14 15:23:28 +0200

    merge

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,17 +1,19 @@
 import { useEffect, useState } from "react";
 import {
-  MapContainer,
-  TileLayer,
-  Marker,
-  Popup,
-  ZoomControl,
-  useMap,
-} from "react-leaflet";
-import { LatLngExpression } from "leaflet";
-import "leaflet/dist/leaflet.css";
-import Spinner from "../Spinner/Spinner";
-import * as L from "leaflet";
-import PopupInfo from "../PopupInfo/PopupInfo";
+	MapContainer,
+	TileLayer,
+	Marker,
+	Popup,
+	ZoomControl,
+	useMap,
+	useMapEvents,
+} from 'react-leaflet';
+import { LatLngExpression } from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import Spinner from '../Spinner/Spinner';
+import * as L from 'leaflet';
+import PopupInfo from '../PopupInfo/PopupInfo';
+import axios from 'axios';
 
 const CustomButton = ({ position }: { position: LatLngExpression }) => {
   const map = useMap();
@@ -44,81 +46,176 @@ const CustomButton = ({ position }: { position: LatLngExpression }) => {
   return null;
 };
 
+const MapClickHandler = ({
+	onMapClick,
+}: {
+	onMapClick: (latlng: [number, number]) => void;
+}) => {
+	useMapEvents({
+		click: (e) => {
+			const { lat, lng } = e.latlng;
+			onMapClick([lat, lng]);
+		},
+	});
+	return null;
+};
+
 const Map = () => {
-  const [position, setPosition] = useState<[number, number] | null>(null);
+	const [position, setPosition] = useState<[number, number] | null>(null);
+	const [markers, setMarkers] = useState<[number, number][]>([]);
+	const [data, setData] = useState<any>(null);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const { latitude, longitude } = position.coords;
-          setPosition([latitude, longitude]);
-        },
-        (error) => {
-          console.error("Error getting geolocation: ", error);
-          setPosition([40.7128, -74.006]);
-        }
-      );
-    } else {
-      alert("Geolocation is not supported by this browser.");
-    }
-  }, []);
+	useEffect(() => {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(
+				(position) => {
+					const { latitude, longitude } = position.coords;
+					setPosition([latitude, longitude]);
+				},
+				(error) => {
+					console.error('Error getting geolocation: ', error);
+					setPosition([40.7128, -74.006]); // Fallback to New York City
+				}
+			);
+		} else {
+			alert('Geolocation is not supported by this browser.');
+		}
+	}, []);
 
-  if (!position) {
-    return <Spinner />;
-  }
+	useEffect(() => {
+		const fetchData = async () => {
+			const latitude = position ? position[0] : null;
+			const longitude = position ? position[1] : null;
 
-  const customDivIcon = L.divIcon({
-    className: "custom-marker",
-    html: `
-     <svg width="36" height="52" viewBox="0 0 36 52" xmlns="http://www.w3.org/2000/svg">
-    <!-- Outer teardrop shape with gradient background -->
-    <path fill="url(#marker-gradient)" fill-rule="evenodd" clip-rule="evenodd" 
-        d="M18 52C18 52 36 34.8302 36 18.1509C36 8.12645 27.9411 0 18 0C8.05887 0 0 8.12645 0 18.1509C0 34.8302 18 52 18 52ZM18 30C24.6274 30 30 24.6274 30 18C30 11.3726 24.6274 6 18 6C11.3726 6 6 11.3726 6 18C6 24.6274 11.3726 30 18 30Z" />
+			if (latitude && longitude) {
+				const url = `https://api.waqi.info/feed/geo:${latitude};${longitude}/?token=${'cab1e7b2713e097b0447eddb77043d80e2515c98'}`;
 
-    <!-- Inner circle remains white -->
-    <circle cx="18" cy="18" r="12" fill="#FFFFFF" />
+				try {
+					const response = await axios.get(url);
+					if (response.data.status === 'ok') {
+						setData(response.data.data);
+					} else {
+						setError('Failed to fetch data');
+					}
+				} catch (err) {
+					console.error(err);
+					setError('Error fetching data');
+				} finally {
+					setLoading(false);
+				}
+			}
+		};
 
-    <!-- Centered number -->
-    <text x="18" y="18" fill="#000000" font-size="10" font-weight="600" 
+		fetchData();
+	}, [position]);
+
+	const getAddress = async (lat: number, lng: number) => {
+		try {
+			const response = await axios.get(
+				`https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lng}&format=json`
+			);
+			if (response.data && response.data.address) {
+				return response.data.address;
+			} else {
+				throw new Error('Address not found');
+			}
+		} catch (error) {
+			console.error('Error getting address:', error);
+			return 'Unknown Address';
+		}
+	};
+
+	if (!position) {
+		return <Spinner />;
+	}
+
+	if (loading) {
+		return <div>Loading...</div>;
+	}
+
+	if (error) {
+		return <div>Error: {error}</div>;
+	}
+
+	const customDivIcon = L.divIcon({
+		className: 'custom-marker',
+		html: `
+      <svg width="36" height="52" viewBox="0 0 36 52" xmlns="http://www.w3.org/2000/svg">
+        <path fill="url(#marker-gradient)" fill-rule="evenodd" clip-rule="evenodd" 
+          d="M18 52C18 52 36 34.8302 36 18.1509C36 8.12645 27.9411 0 18 0C8.05887 0 0 8.12645 0 18.1509C0 34.8302 18 52 18 52ZM18 30C24.6274 30 30 24.6274 30 18C30 11.3726 24.6274 6 18 6C11.3726 6 6 11.3726 6 18C6 24.6274 11.3726 30 18 30Z" />
+        <circle cx="18" cy="18" r="12" fill="#FFFFFF" />
+        <text x="18" y="18" fill="#000000" font-size="10" font-weight="600" 
           text-anchor="middle" dominant-baseline="middle">7</text>
+        <defs>
+          <linearGradient id="marker-gradient" gradientTransform="rotate(180)">
+            <stop offset="0%" stop-color="#ACF254" />
+            <stop offset="100%" stop-color="#20944E" />
+          </linearGradient>
+        </defs>
+      </svg>
+    `,
+		iconSize: [36, 52],
+		iconAnchor: [18, 52],
+		popupAnchor: [0, 300],
+	});
 
-    <!-- Gradient definition -->
-    <defs>
-        <linearGradient id="marker-gradient" gradientTransform="rotate(180)">
-            <stop offset="0%" stop-color="#ACF254" /> <!-- Light green at the top -->
-            <stop offset="100%" stop-color="#20944E" /> <!-- Dark green at the bottom -->
-        </linearGradient>
-    </defs>
-</svg>
+	const handleMapClick = async (latlng: [number, number]) => {
+		if (latlng && latlng[0] !== undefined && latlng[1] !== undefined) {
+			const address = await getAddress(latlng[0], latlng[1]);
+			setMarkers((prev) => [...prev, latlng]);
+			return address;
+		} else {
+			console.error('Invalid coordinates:', latlng);
+		}
+	};
 
+	return (
+		<div style={{ height: `93vh`, width: '100%', zIndex: 1 }}>
+			<MapContainer
+				center={position as LatLngExpression}
+				zoom={25}
+				style={{ height: '100%', width: '100%' }}
+				zoomControl={false}
+			>
+				<ZoomControl position='bottomright' />
+				<TileLayer url='https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png' />
 
+				<Marker
+					position={position as LatLngExpression}
+					icon={customDivIcon}
+				>
+					<Popup>
+						<PopupInfo
+							position={position}
+							getAddress={handleMapClick}
+							data={data}
+						/>
+					</Popup>
+				</Marker>
 
-`,
-    iconSize: [36, 52],
-    iconAnchor: [18, 52],
-    popupAnchor: [0, 300],
-  });
+				{markers.map((marker, index) => (
+					<Marker
+						key={index}
+						position={marker}
+						icon={customDivIcon}
+					>
+						<Popup>
+							<PopupInfo
+								position={marker}
+								getAddress={handleMapClick}
+								data={data}
+							/>
+						</Popup>
+					</Marker>
+				))}
 
-  return (
-    <div style={{ height: `93vh`, width: "100%", zIndex: 1 }}>
-      <MapContainer
-        center={position as LatLngExpression}
-        zoom={25}
-        style={{ height: "100%", width: "100%" }}
-        zoomControl={false}
-      >
-        <ZoomControl position="bottomright" />
-        <TileLayer url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png" />
-        <Marker position={position} icon={customDivIcon}>
-          <Popup>
-            <PopupInfo position={position} />
-          </Popup>
-        </Marker>
-        <CustomButton position={position as LatLngExpression} />
-      </MapContainer>
-    </div>
-  );
+				<CustomButton position={position as LatLngExpression} />
+				<MapClickHandler onMapClick={handleMapClick} />
+			</MapContainer>
+		</div>
+	);
 };
 
 export default Map;
--- src/components/PopupInfo/PopupInfo.tsx
+++ src/components/PopupInfo/PopupInfo.tsx
@@ -1,10 +1,17 @@
 import { LatLngExpression, LatLng } from 'leaflet';
+import { useEffect, useState } from 'react';
 
 interface PopupInfoProps {
 	position: LatLngExpression | undefined;
+	getAddress: any;
+	data: any;
 }
 
-export default function PopupInfo({ position }: PopupInfoProps) {
+export default function PopupInfo({
+	position,
+	getAddress,
+	data,
+}: PopupInfoProps) {
 	if (!position) {
 		return <p>Location not available</p>;
 	}
@@ -21,11 +28,26 @@ export default function PopupInfo({ position }: PopupInfoProps) {
 		return <p>Invalid position</p>;
 	}
 
+	const [address, setAddress] = useState<any>(null);
+
+	useEffect(() => {
+		const fetchAddress = async () => {
+			try {
+				const resolvedAddress = await getAddress(position);
+				setAddress(resolvedAddress);
+			} catch (error) {
+				console.error('Error fetching address:', error);
+			}
+		};
+
+		fetchAddress();
+	}, []);
+
 	return (
 		<div className='info'>
 			<div className='info__top'>
 				<div className='info__top__left'>
-					<span className='info__top__left__val'>7.8</span>
+					<span className='info__top__left__val'>{data.iaqi.pm10.v}</span>
 					<span className='info__top__left__pm'>PM2.5</span>
 				</div>
 				<div className='info__top__right'>
@@ -39,14 +61,13 @@ export default function PopupInfo({ position }: PopupInfoProps) {
 						>
 							<path
 								fill='#ffffff'
-								fill-rule='evenodd'
-								clip-rule='evenodd'
+								fillRule='evenodd'
+								clipRule='evenodd'
 								d='M18 52C18 52 36 34.8302 36 18.1509C36 8.12645 27.9411 0 18 0C8.05887 0 0 8.12645 0 18.1509C0 34.8302 18 52 18 52ZM18 30C24.6274 30 30 24.6274 30 18C30 11.3726 24.6274 6 18 6C11.3726 6 6 11.3726 6 18C6 24.6274 11.3726 30 18 30Z'
 							/>
 						</svg>
 						<div className='info__top__right__address__name'>
-							Diagon Alley, 12
-							<br /> 1 floor, 2 companies
+							{address?.road} {address?.house_number}
 						</div>
 					</div>
 				</div>
@@ -54,15 +75,17 @@ export default function PopupInfo({ position }: PopupInfoProps) {
 			<div className='info__bottom'>
 				<div className='info__bottom__box'>
 					<span className='info__bottom__box__name'>CO2</span>
-					<span className='info__bottom__box__value'>685.5</span>
+					<span className='info__bottom__box__value'>{data.iaqi.p.v}</span>
 				</div>
 				<div className='info__bottom__box'>
 					<span className='info__bottom__box__name'>Temp</span>
-					<span className='info__bottom__box__value'>13&deg;C</span>
+					<span className='info__bottom__box__value'>
+						{data.iaqi.t.v}&deg;C
+					</span>
 				</div>
 				<div className='info__bottom__box'>
 					<span className='info__bottom__box__name'>Metrics</span>
-					<span className='info__bottom__box__value'>444.2</span>
+					<span className='info__bottom__box__value'>{data.idx}</span>
 				</div>
 			</div>
 		</div>
